b5c114737b72599915fb40ac34b68487e25b26ea
Meneralka <frutash32@gmail.com>
2024-05-18 13:25:44 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 # - coding: utf-8 -*-
 # !/usr/bin/python3
-#         ||\\  //||  ||\\\\] ||\\    ||  ||\\\\] ||\\\]  ||\\\\||  ||       ||  //  ||\\\\||
-#         || \\// ||  ||      || \\   ||  ||      ||  ||  ||    ||  ||       || //   ||    ||
-#         ||  \/  ||  ||\\\]  ||  \\  ||  ||\\\]  ||\\\]  ||\\\\||  ||       ||<<    ||\\\\||
-#         ||      ||  ||      ||   \\ ||  ||      ||\\    ||    ||  ||       || \\   ||    ||
-#         ||      ||  ||\\\]  ||    \\||  ||\\\]  || \\   ||    ||  ||\\\\]  ||  \\  ||    ||
-#
 #         version: 1.0.0/29.12.20
 #
+
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import functionsClass
 
 
 # ------------- [ CONNECT TO VK ] -----------
-token = "deb38b62e904d6a1fa520467e43314cb25b885448de2634ea0a88bd57fa9121ba5633e461a9b7b29be845"  # token group
-group_id = "195885210"  # ID group
+token = ''  # token group
+group_id = ""  # ID group
 vk_session = vk_api.VkApi(token=token)  # Обработка access_token
 
 longpoll = VkBotLongPoll(vk_session, group_id)  # Данные для работы в сообществе
@@ -23,8 +18,6 @@ vk = vk_session.get_api()  # For api requests
 
 
 # ----------------[ ENGINE ]-----------------
-admins = [274868427, 553863104, 308137930, 553499842, 278652149, 553069569, 563663518]
-
 print("The bot is running")
 
 while True:
